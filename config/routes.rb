a4718d0ac21d9e57c2aa51ff54060879b6542ec8
nasakomu <nasakomunasakomu@gmail.com>
2020-07-01 16:25:51 +0900

    add user-edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root to:'application#top'
-  get "/login",to: 'application#login'
-  get "/signup",to: 'application#signup'
   get "/recipe",to: 'application#recipe'
+  resources :users, only: [:edit, :update]
   
 end
